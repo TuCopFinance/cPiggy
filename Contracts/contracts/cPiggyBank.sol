1445eca0c903ffad782763aa8477a0b57472ec76
Krisztian Barta <krisztian@galaxis.xyz>
2025-07-16 10:34:14 +0200

    Added deposit function

--- Contracts/contracts/cPiggyBank.sol
+++ Contracts/contracts/cPiggyBank.sol
@@ -11,20 +11,20 @@ contract cPiggyBank is Ownable, ReentrancyGuard {
     using SafeERC20 for IERC20;
 
     struct PiggyDeposit {
-        uint256 amount;         // Original cCOP deposited
-        uint256 startTime;      // deposit time
-        uint256 unlockTime;     // when user can claim tokens back
-        uint256 duration;       // lock period in seconds
-        bool claimed;           // whether it is withdrawn or not
-        bool safeMode;          // if used conservative allocation
-    }    
+        uint256 amount; // Original cCOP deposited
+        uint256 startTime; // deposit time
+        uint256 unlockTime; // when user can claim tokens back
+        uint256 duration; // lock period in seconds
+        bool claimed; // whether it is withdrawn or not
+        bool safeMode; // if used conservative allocation
+    }
 
     IERC20 public immutable cCOP;
     IERC20 public cUSD;
     IERC20 public cREAL;
 
-    address public swapRouter;      // address of Mento swap Router
-    address public oracle;          // FX oracle source
+    address public swapRouter; // address of Mento swap Router
+    address public oracle; // FX oracle source
 
     uint256 public constant DURATION_30 = 30 days;
     uint256 public constant DURATION_60 = 60 days;
@@ -32,10 +32,19 @@ contract cPiggyBank is Ownable, ReentrancyGuard {
 
     mapping(address => PiggyDeposit[]) public userDeposits;
 
-    event Deposited(address indexed user, uint256 amount, uint256 duration, bool safeMode);
+    event Deposited(
+        address indexed user,
+        uint256 amount,
+        uint256 duration,
+        bool safeMode
+    );
     event Withdrawn(address indexed user, uint256 amount, uint256 reward);
 
-    constructor(address _cCOP, address _cUSD, address _cREAL) Ownable (msg.sender){
+    constructor(
+        address _cCOP,
+        address _cUSD,
+        address _cREAL
+    ) Ownable(msg.sender) {
         require(_cCOP != address(0), "Invalid cCOP address");
         require(_cUSD != address(0), "Invalid cUSD address");
         require(_cREAL != address(0), "Invalud cReal address");
@@ -44,5 +53,41 @@ contract cPiggyBank is Ownable, ReentrancyGuard {
         cUSD = IERC20(_cUSD);
         cREAL = IERC20(_cREAL);
     }
+
+    function deposit(
+        uint256 amount,
+        uint256 duration,
+        bool safeMode
+    ) external nonReentrant {
+        require(amount > 0, "Amount must be greater than 0");
+        require(
+            duration == DURATION_30 ||
+                duration == DURATION_60 ||
+                duration == DURATION_90,
+            "Invalid lock duration"
+        );
+
+        // Transfer cCOP from user to contract
+
+        //TODO: Probably need approve somehow (maybe only in frontend)
+        cCOP.safeTransferFrom(msg.sender, address(this), amount);
+
+        uint256 unlockTime = block.timestamp + duration;
+
+        // Store user deposit
+        PiggyDeposit memory newDeposit = PiggyDeposit({
+            amount: amount,
+            startTime: block.timestamp,
+            unlockTime: unlockTime,
+            duration: duration,
+            claimed: false,
+            safeMode: safeMode
+        });
+
+        userDeposits[msg.sender].push(newDeposit);
+
+        emit Deposited(msg.sender, amount, duration, safeMode);
+    }
+
 
 }
