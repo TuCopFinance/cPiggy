98b1813bfc25f2cf5926d1aa72991f14787c94ed
Krisztian Barta <krisztian@galaxis.xyz>
2025-07-19 14:33:41 +0200

    reown integration

--- frontend/src/constants/Contracts.tsx
+++
@@ -1,5 +0,0 @@
-import type { Abi } from 'viem'
-import piggyAbiJson from '../../../Contracts/artifacts/contracts/cPiggyBank.sol/cPiggyBank.json'
-
-export const PIGGYBANK_ADDRESS = '0x80B871DF504978707DE1745357493a67Ce3d61A0'
-export const abi = piggyAbiJson.abi as Abi
